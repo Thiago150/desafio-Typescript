76de88c03b808ce82510fba5d50f5111fa2c5ba2
Thiago Lopes <thiagolnunes.2018@outlook.com>
2022-06-22 12:01:23 -0300

    desafio de typescrip da digital inovation one

---
+++ README.md
@@ -0,0 +1,2 @@
+# Desafio de TypeScript!
+## foram 4 desafios propostos na aula.
---
+++ src/desafios/desafio3.ts
@@ -0,0 +1,22 @@
+let botaoAtualizar = document.getElementById('atualizar-saldo') as HTMLButtonElement;
+let botaoLimpar = document.getElementById('limpar-saldo') as HTMLButtonElement;
+let soma = document.getElementById('soma') as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo') as HTMLSpanElement;
+
+campoSaldo.innerHTML = '0';
+
+function somarAoSaldo(soma:number) {
+    campoSaldo.innerHTML = (Number(campoSaldo.innerHTML) + soma).toString();
+}
+
+function limparSaldo() {
+    campoSaldo.innerHTML = '0';
+}
+
+botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(Number(soma.value));
+});
+
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
